7d82e0c242edc9ecfcfa023b681e79b2a2d34e63
LW Htet Aung <kiritokirikaya80@gmail.com>
2023-10-09 22:58:57 +0630

    dashboard sidebar layout

---
+++ app/(dashboard)/_components/SidebarItem.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+interface SidebarItemProps {
+    icon: string;
+    label:string;
+}
+
+const SidebarItem = ({icon,label,href}) => {
+  return (
+    <div>SidebarItem</div>
+  )
+}
+export default SidebarItem
