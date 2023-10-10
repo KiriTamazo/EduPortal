e2111c986a22fc644e26e8259174e0ed6ac4065a
LW Htet Aung <kiritokirikaya80@gmail.com>
2023-10-10 23:07:12 +0630

    add sidebar

--- app/(dashboard)/_components/SidebarItem.tsx
+++ app/(dashboard)/_components/SidebarItem.tsx
@@ -1,13 +1,32 @@
 'use client'
 
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
 interface SidebarItemProps {
-    icon: string;
+    icon: LucideIcon;
     label:string;
+    href:string;
+
 }
 
-const SidebarItem = ({icon,label,href}) => {
+const SidebarItem = ({icon:Icon,label,href}:SidebarItemProps) => {
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const isActive  = (pathName === '/' && href ==='/') || (pathName === href || pathName?.startsWith(`${href}`));
+  const onClick = () => router.push(href)
   return (
-    <div>SidebarItem</div>
+    <button onClick={onClick} type="button" className={cn('flex items-center gap-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20',isActive && 'text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700')}>
+      
+      <div className="flex items-center gap-x-2 py-4">
+        <Icon 
+        size={22}
+        className={cn('text-slate-500',isActive && 'text-sky-700')}
+        />
+      </div>
+      SidebarItem</button>
   )
 }
 export default SidebarItem
